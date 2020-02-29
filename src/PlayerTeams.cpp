42ba1570ad631b9608c065f62e329464981bf856
Thiago Paiva <thiago.t_27@hotmail.com>
2020-02-29 07:48:10 -0300

    Testing confidence

--- src/PlayerTeams.cpp
+++ src/PlayerTeams.cpp
@@ -220,6 +220,7 @@ SoccerCommand Player::erus_midfielder(  )
   {
     formations->setFormation( ERUS_DEFAULT_FORMATION );
     soc.commandType = CMD_ILLEGAL;
+    PS->setPlayerConfThr(0.6);
 
     if( WM->getConfidence( OBJECT_BALL ) < PS->getBallConfThr() )
     {
